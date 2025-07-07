76b457658ebd528194dc14adcf93f0d5344dfa5e
manntrr <man22057@byui.edu>
2025-07-07 15:18:26 -0400

    completed stacks individual work

--- week02/analyze/MysteryStack1.cs
+++ week02/analyze/MysteryStack1.cs
@@ -1,5 +1,13 @@
-﻿public static class MysteryStack1 {
-    public static string Run(string text) {
+﻿public static class MysteryStack1
+{
+    // this will reverse the characters in a string 
+
+    // expected output for # 2
+    // racecar ==> racecar
+    // stressed ==> desserts
+    // a nut for a jar of tuna ==> anut fo raj a rof tun a
+    public static string Run(string text)
+    {
         var stack = new Stack<char>();
         foreach (var letter in text)
             stack.Push(letter);
--- week02/analyze/MysteryStack2.cs
+++ week02/analyze/MysteryStack2.cs
@@ -1,28 +1,47 @@
-﻿public static class MysteryStack2 {
-    private static bool IsFloat(string text) {
+﻿public static class MysteryStack2
+{
+
+    // this is a postfix basic mathematics expression evaluation routine
+    // #2 results
+    //      5 3 7 + * ==> 5 10 * ==> 50
+    //      6 2 + 5 3 - / ==> 8 5 3 - / ==> 8 2 / ==> 4
+    // #3 inputs for errors:
+    //      "invalid case 1" ==> 1 + 2
+    //      "invalid case 2" ==> 1 0 /
+    //      "invalid case 3" ==> 1 ^ 2
+    //      "invalid case 4" ==> 0 1 1 -
+    private static bool IsFloat(string text)
+    {
         return float.TryParse(text, out _);
     }
 
-    public static float Run(string text) {
+    public static float Run(string text)
+    {
         var stack = new Stack<float>();
-        foreach (var item in text.Split(' ')) {
-            if (item == "+" || item == "-" || item == "*" || item == "/") {
+        foreach (var item in text.Split(' '))
+        {
+            if (item == "+" || item == "-" || item == "*" || item == "/")
+            {
                 if (stack.Count < 2)
                     throw new ApplicationException("Invalid Case 1!");
 
                 var op2 = stack.Pop();
                 var op1 = stack.Pop();
                 float res;
-                if (item == "+") {
+                if (item == "+")
+                {
                     res = op1 + op2;
                 }
-                else if (item == "-") {
+                else if (item == "-")
+                {
                     res = op1 - op2;
                 }
-                else if (item == "*") {
+                else if (item == "*")
+                {
                     res = op1 * op2;
                 }
-                else {
+                else
+                {
                     if (op2 == 0)
                         throw new ApplicationException("Invalid Case 2!");
 
@@ -31,12 +50,15 @@
 
                 stack.Push(res);
             }
-            else if (IsFloat(item)) {
+            else if (IsFloat(item))
+            {
                 stack.Push(float.Parse(item));
             }
-            else if (item == "") {
+            else if (item == "")
+            {
             }
-            else {
+            else
+            {
                 throw new ApplicationException("Invalid Case 3!");
             }
         }
